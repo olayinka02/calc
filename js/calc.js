fa7ed9a1bc6dc119d8ab508fb2c6ebf971bd3f14
Omolola <53899116+motunrayo01@users.noreply.github.com>
2020-05-04 09:36:41 -0300

    simple scientific calculator
    this calculator was designe with html, css and javascript

---
+++ js/calc.js
@@ -0,0 +1,41 @@
+function one(){
+    var element1= document.getElementById('one').value;
+     document.getElementById("templates").innerHTML=element1;
+ }
+ function two(){
+     var element2= document.getElementById('two').value;
+     document.getElementById("templates").innerHTML=element2;
+ }
+ function three(){
+     var element3=document.getElementById('three').value;
+     document.getElementById("templates").innerHTML=element3;
+ }
+ function four(){
+    var element4= document.getElementById('four').value;
+     document.getElementById("templates").innerHTML=element4;
+ }
+ function five(){
+     var element5= document.getElementById('five').value;
+     document.getElementById("templates").innerHTML=element5;
+ }
+ function six(){
+     var element6=document.getElementById('six').value;
+     document.getElementById("templates").innerHTML=element6;
+ }
+ function seven(){
+    var element7= document.getElementById('seven').value;
+     document.getElementById("templates").innerHTML=element7;
+ }
+ function eight(){
+     var element8= document.getElementById('eight').value;
+     document.getElementById("templates").innerHTML=element8;
+ }
+ function nine(){
+     var element9=document.getElementById('nine').value;
+     document.getElementById("templates").innerHTML=element9;
+ }
+ function zero(){
+    var element0= document.getElementById('zero').value;
+     document.getElementById("templates").innerHTML=element0;
+ }
+ 
